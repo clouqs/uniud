725644e8914a3443f5e7c836f2d1a17c03422ec3
clouqs <155663572+clouqs@users.noreply.github.com>
2024-10-09 11:47:40 +0200

    Add files via upload

---
+++ Lezioni Racket/lezione 09.10.24.rkt
@@ -0,0 +1,27 @@
+#lang scheme
+(define sup-tot-cilindro ; val: reale
+  (lambda (r h)      ; r,h : reali (misure)
+    (* (* (* 2 pi) r) (+ r h)) ; formula
+    ))
+
+
+
+(define plurale-sm        ; val: stringa
+  (lambda (sm)            ; sm: stringa
+    (string-append (radice-sost sm) "i")))
+
+
+(define radice-sost        ;val: stringa
+  (lambda (s)              ; s: stringa
+    (substring s 0 (- (string-length s) 1))  ;(string=? (substring s 0 (- (string-length s) 1) restituisce un valore booleano
+              ))
+
+
+
+
+;--------------------------------------------------------------------------------------;
+;  ; (string=? (substring s (- (string-length s) 1) (string-length s)) "a")
+
+(define  femminile?     ; val: booleano (T or F)
+  (lambda (s) ; s: stringa
+    (char=? (string-ref s (- (string-length s) 1)) #\a)))
