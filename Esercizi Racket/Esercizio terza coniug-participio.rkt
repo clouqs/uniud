3aeaaf095efc7c5163ca8b4bd98e5d9c26acad87
clouqs <155663572+clouqs@users.noreply.github.com>
2024-10-16 09:41:17 +0200

    Update Esercizio terza coniug-participio.rkt

--- Esercizi Racket/Esercizio terza coniug-participio.rkt
+++ Esercizi Racket/Esercizio terza coniug-participio.rkt
@@ -1,6 +1,3 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |Esercizio terza coniug-participio|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;; conversione verbi -ere al participio passato
 ;; verbi regolari, NON irregolari
 (define radice-verb
