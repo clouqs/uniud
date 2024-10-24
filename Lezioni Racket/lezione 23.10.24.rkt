dda953c000d1823967e9195ec6236b6880c4fd1c
clouqs <155663572+clouqs@users.noreply.github.com>
2024-10-24 08:38:14 +0200

    Update lezione 23.10.24.rkt

--- Lezioni Racket/lezione 23.10.24.rkt
+++ Lezioni Racket/lezione 23.10.24.rkt
@@ -1,6 +1,3 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |lezione 23.10.24|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;-----complemento a uno con il cond-----;
 (define bit-compl   ;val: stringa "0" o "1" 
   (lambda (bit)     ;bit: stringa "0" o "1"
