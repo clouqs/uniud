67764d06178855528d0a903f451035172633aba4
clouqs <155663572+clouqs@users.noreply.github.com>
2024-10-29 12:15:55 +0100

    Update Esercizio 1 laboratorio... frase maxy.rkt

--- Esercizi Racket/Esercizio 1 laboratorio... frase maxy.rkt
+++ Esercizi Racket/Esercizio 1 laboratorio... frase maxy.rkt
@@ -1,6 +1,3 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |esercizio frase|) (read-case-sensitive #t) (teachpacks ((lib "drawings.ss.txt" "installed-teachpacks"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "drawings.ss.txt" "installed-teachpacks")) #f)))
 (define frase
   (lambda (soggetto predicato-verbale oggetto)
     (string-append
